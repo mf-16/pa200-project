f9427ee93117439395088c5e18be2a1439f01bbb
Matúš Formánek <formanekmato@gmail.com>
2024-10-10 23:21:25 +0200

    feat: created DBContext

---
+++ DataAccessLayer/Data/BookHubDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DataAccessLayer.Data;
+
+public class BookHubDbContext : DbContext
+{
+    public BookHubDbContext(DbContextOptions options) : base(options)
+    {
+    }
+}
