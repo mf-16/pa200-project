d2f6bf1aab0cf31133820b3da727d13624d2fd9c
Matúš Formánek <formanekmato@gmail.com>
2024-10-13 18:37:18 +0200

    feat: created user, wishlist, role and review models

---
+++ DataAccessLayer/Model/User.cs
@@ -0,0 +1,8 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace DataAccessLayer.Model;
+
+public class User : IdentityUser<int>
+{
+    
+}
---
+++ DataAccessLayer/Model/UserRole.cs
@@ -0,0 +1,8 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace DataAccessLayer.Model;
+
+public class UserRole: IdentityRole<int>
+{
+    
+}
