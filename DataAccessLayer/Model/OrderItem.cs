09d1030182bdedc862d1744ebff51d450a6b6079
xforman2 <xforman2@fi.muni.cz>
2024-10-14 14:34:37 +0200

    feat: add Order and OrderItem

--- DataAccessLayer/Model/OrderItem.cs
+++ DataAccessLayer/Model/OrderItem.cs
@@ -1,3 +1,17 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace DataAccessLayer.Model;
 
-public class OrderItem : BaseEntity { }
+public class OrderItem : BaseEntity
+{
+    public int OrderId { get; set; }
+
+    [ForeignKey("OrderId")]
+    public Order Order { get; set; }
+    public int BookId { get; set; }
+
+    [ForeignKey("BookId")]
+    public Book Book { get; set; }
+    public int Quantity { get; set; }
+    public decimal Price { get; set; }
+}
