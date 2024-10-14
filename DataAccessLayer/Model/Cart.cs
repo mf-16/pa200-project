011c199f3cae4e96ab6a48514edfeb075276cb63
xforman2 <xforman2@fi.muni.cz>
2024-10-14 14:34:18 +0200

    feat: add Cart and CartItem

--- DataAccessLayer/Model/Cart.cs
+++ DataAccessLayer/Model/Cart.cs
@@ -1,3 +1,12 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace DataAccessLayer.Model;
 
-public class Cart : BaseEntity { }
+public class Cart : BaseEntity
+{
+    public int UserId { get; set; }
+
+    [ForeignKey("UserId")]
+    public User User { get; set; }
+    public virtual ICollection<CartItem> CartItems { get; set; }
+}
--- DataAccessLayer/Model/CartItem.cs
+++ DataAccessLayer/Model/CartItem.cs
@@ -1,3 +1,16 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace DataAccessLayer.Model;
 
-public class CartItem : BaseEntity { }
+public class CartItem : BaseEntity
+{
+    public int OrderId { get; set; }
+
+    [ForeignKey("OrderId")]
+    public Order Order { get; set; }
+    public int BookId { get; set; }
+
+    [ForeignKey("BookId")]
+    public Book Book { get; set; }
+    public int Quantity { get; set; }
+}
