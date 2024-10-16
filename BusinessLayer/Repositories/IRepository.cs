f4ec8d1f109dbd7024e690597425089b7d2bab22
xforman2 <xforman2@fi.muni.cz>
2024-10-16 13:38:13 +0200

    feat: repository pattern

---
+++ BusinessLayer/Repositories/IRepository.cs
@@ -0,0 +1,11 @@
+namespace BusinessLayer.Repositories;
+
+public interface IRepository<T>
+    where T : class
+{
+    void Add(T entity);
+    void Delete(T entity);
+    void Update(T entity);
+    Task<T?> GetById(int id);
+    Task<IEnumerable<T>> GetAll();
+}
---
+++ DataAccessLayer/Repositories/GenericRepository.cs
@@ -0,0 +1,41 @@
+using BusinessLayer.Repositories;
+using DataAccessLayer.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccessLayer.Repositories;
+
+public class GenericRepository<T> : IRepository<T>
+    where T : class
+{
+    private readonly DbSet<T> _entities;
+
+    public GenericRepository(BookHubDbContext context)
+    {
+        _entities = context.Set<T>();
+    }
+
+    public async void Add(T entity)
+    {
+        await _entities.AddAsync(entity);
+    }
+
+    public void Delete(T entity)
+    {
+        _entities.Remove(entity);
+    }
+
+    public void Update(T entity)
+    {
+        _entities.Update(entity);
+    }
+
+    public async Task<T?> GetById(int id)
+    {
+        return await _entities.FindAsync(id);
+    }
+
+    public async Task<IEnumerable<T>> GetAll()
+    {
+        return await _entities.ToListAsync();
+    }
+}
