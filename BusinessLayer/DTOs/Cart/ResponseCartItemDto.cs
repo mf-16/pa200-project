903722f0121ee6193cbdf0b5edf5f3a838248d11
Matúš Formánek <formanekmato@gmail.com>
2024-10-18 17:04:46 +0200

    feat: added ids to response dtos

--- BusinessLayer/DTOs/Cart/ResponseCartItemDto.cs
+++ BusinessLayer/DTOs/Cart/ResponseCartItemDto.cs
@@ -2,6 +2,7 @@ namespace BusinessLayer.DTOs.Cart;
 
 public class ResponseCartItemDto
 {
+    public int Id { get; set; }
     public string BookTitle { get; set; }
     public decimal BookPrice { get; set; }
     public int Quantity { get; set; }
--- BusinessLayer/DTOs/Order/ResponseOrderItemDto.cs
+++ BusinessLayer/DTOs/Order/ResponseOrderItemDto.cs
@@ -2,6 +2,7 @@ namespace BusinessLayer.DTOs.Order;
 
 public class ResponseOrderItemDto
 {
+    public int Id { get; set; }
     public decimal BookPrice { get; set; }
     public string BookTitle { get; set; }
     public int Quantity { get; set; }
