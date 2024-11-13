a65f953fa6b17840b51be92312a5623bc66d00cd
Matúš Formánek <formanekmato@gmail.com>
2024-11-13 16:41:30 +0100

    fix: login view model

---
+++ WebMVC/Models/LoginViewModel.cs
@@ -0,0 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace WebMVC.Models;
+
+public class LoginViewModel
+{
+    [Required]
+    public string Email { get; set; }
+
+    [Required]
+    [DataType(DataType.Password)]
+    public string Password { get; set; }
+
+    [Display(Name = "Remember me?")]
+    public bool RememberMe { get; set; }
+}
