9e6c2a2ac82184b8544e11b804cec6eb1f95ed04
Matúš Formánek <formanekmato@gmail.com>
2024-12-08 23:38:12 +0100

    refactor : refactor Login + Register views, added isAdmin checkbox

--- BusinessLayer/Services/AuthService.cs
+++ BusinessLayer/Services/AuthService.cs
@@ -10,24 +10,57 @@ public class AuthService : IAuthService
 {
     private readonly UserManager<User> _userManager;
     private readonly SignInManager<User> _signInManager;
+    private readonly RoleManager<UserRole> _roleManager;
     private readonly IMapper _mapper;
 
     public AuthService(
         UserManager<User> userManager,
         SignInManager<User> signInManager,
-        IMapper mapper
-    )
+        IMapper mapper, RoleManager<UserRole> roleManager)
     {
         _userManager = userManager;
         _signInManager = signInManager;
         _mapper = mapper;
+        _roleManager = roleManager;
     }
 
     public async Task<IdentityResult> RegisterAsync(RegisterDTO registerDto)
     {
         var user = _mapper.Map<RegisterDTO, User>(registerDto);
 
-        return await _userManager.CreateAsync(user, registerDto.Password);
+        var result =  await _userManager.CreateAsync(user, registerDto.Password);
+        
+        if (result.Succeeded)
+        {
+            if (registerDto.IsAdmin)
+            {
+                var adminRoleExists = await _roleManager.RoleExistsAsync("Admin");
+                if (!adminRoleExists)
+                {
+                    await _roleManager.CreateAsync(new UserRole()
+                    {
+                        Name = "Admin",
+                    });
+                }
+
+                await _userManager.AddToRoleAsync(user, "Admin");
+            }
+            else
+            {
+                var userRoleExists = await _roleManager.RoleExistsAsync("User");
+                if (!userRoleExists)
+                {
+                    await _roleManager.CreateAsync(new UserRole()
+                    {
+                        Name = "User",
+                    });
+                }
+
+                await _userManager.AddToRoleAsync(user, "User");
+            }
+        }
+
+        return result;
     }
 
     public async Task<SignInResult> LoginAsync(LoginDTO loginDto)
